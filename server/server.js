f2d6bcb14bef047c425b5573a6dcde3e15f8ed32
Shreyas <shreyasbandekar01@gmail.com>
2025-09-02 19:54:25 +0530

    Fix route path for user API to ensure consistency

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 connectDB();
 
 // Routes 
-app.use('/api/users', userRouter);
+app.use('/api/user', userRouter);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
