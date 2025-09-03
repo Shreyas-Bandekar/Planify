e508de3fed3f082ffbfbdde1f9ecf7a6cb59f596
Shreyas <shreyasbandekar01@gmail.com>
2025-09-03 18:27:11 +0530

    Implement task model with schema definition for title, description, priority, due date, owner, status, and createdAt fields

---
+++ server/models/taskModel.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({    
+    title:{
+        type: String,
+        required: true  
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    priority:{
+        type: String,
+        enum: ["low", "medium", "high"],
+        default: "low"
+    },
+    dueDate: {
+        type: Date,
+        required: true
+    },
+    owner:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["pending", "in-progress", "completed"],
+        default: "pending"
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Task = mongoose.model.Task || mongoose.model("Task", taskSchema);
+
+export default Task;
