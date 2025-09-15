33ecec7f6905138c84dde077c20071420d5a275c
Shreyas <shreyasbandekar01@gmail.com>
2025-09-15 19:10:36 +0530

    Fix formatting in userRoute.js for improved readability

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {registerUser,loginUser,getCurrentUser,updateProfile,changePassword } from '../controllers/userController.js';
+import { registerUser, loginUser, getCurrentUser, updateProfile, changePassword } from '../controllers/userController.js';
 import authMiddleware from '../middlewares/auth.js';
 
 const userRouter = express.Router();
@@ -9,8 +9,8 @@ userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
 
 // Private Route protect middleware also
-userRouter.get('/me',authMiddleware, getCurrentUser);
-userRouter.put('/profile',authMiddleware, updateProfile);
-userRouter.put('/password',authMiddleware, changePassword);
+userRouter.get('/me', authMiddleware, getCurrentUser);
+userRouter.put('/profile', authMiddleware, updateProfile);
+userRouter.put('/password', authMiddleware, changePassword);
 
 export default userRouter;
