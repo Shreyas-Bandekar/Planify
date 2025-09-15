2a07ed5986943634e2e5bd47520a9ead2251c737
Shreyas <shreyasbandekar01@gmail.com>
2025-09-15 19:10:13 +0530

    Fix formatting in authMiddleware for improved readability

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -6,8 +6,8 @@ const JWT_Secret = process.env.JWT_SECRET;
 export default async function authMiddleware(req, res, next) {
     // get bearer token from authorization header
     const authHeader = req.headers.authorization;
-    if(!authHeader || !authHeader.startsWith("Bearer ")) {
-        return res.status(401).json({success: false, message: "Unauthorized"});
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ success: false, message: "Unauthorized" });
     }
 
     const token = authHeader.split(" ")[1];
@@ -15,13 +15,13 @@ export default async function authMiddleware(req, res, next) {
         const payload = jwt.verify(token, JWT_Secret);
         const user = await User.findById(payload.id).select("-password");
 
-        if(!user) {
-            return res.status(404).json({success: false, message: "User not found"});
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found" });
         }
         req.user = user;
         next();
     } catch (error) {
         console.log(error);
-        res.status(401).json({success: false, message: "Unauthorized"});
+        res.status(401).json({ success: false, message: "Unauthorized" });
     }
 }
