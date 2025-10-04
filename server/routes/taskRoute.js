bff67c3e5726adac2031d2d4203ce66853f205b7
Shreyas <shreyasbandekar01@gmail.com>
2025-10-04 13:41:04 +0530

    Fix formatting in taskRoute.js for improved readability

--- server/routes/taskRoute.js
+++ server/routes/taskRoute.js
@@ -4,8 +4,8 @@ import authMiddleware from '../middlewares/auth.js';
 
 const taskRouter = express.Router();
 
-taskRouter.route('/gp').get(authMiddleware,getTasks).post(authMiddleware,createTask);
-taskRouter.route('/gp/:id').get(authMiddleware,getTaskById).put(authMiddleware,updateTask).delete(authMiddleware,deleteTask);
+taskRouter.route('/gp').get(authMiddleware, getTasks).post(authMiddleware, createTask);
+taskRouter.route('/gp/:id').get(authMiddleware, getTaskById).put(authMiddleware, updateTask).delete(authMiddleware, deleteTask);
 taskRouter.route('/bulk').post(authMiddleware, bulkCreateTasks);
 taskRouter.route('/stats').get(authMiddleware, getTaskStats);
 
